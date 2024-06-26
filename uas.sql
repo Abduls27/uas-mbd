0b64d0962fecd4fb8a4e93793ae645b2a9637512
Abduls27 <fahmiabdullah628@gmail.com>
2024-06-26 21:29:45 +0700

    modified:   uas.sql

--- uas.sql
+++ uas.sql
@@ -1,5 +1,27 @@
+-- Trigger untuk mengatur tanggal saat insert data pada tabel modules
 DELIMITER $$
 CREATE TRIGGER `tanggal` BEFORE INSERT ON `modules`
+ FOR EACH ROW 
+ BEGIN
+  SET NEW.created_at = NOW();
+  SET NEW.updated_at = NOW();
+END $$
+DELIMITER ;
+
+-- trigger untuk mengatur tanggal saat insert data pada tabel submodules
+DELIMITER $$
+CREATE TRIGGER `tanggal-submodules` BEFORE INSERT ON `submodules`
+ FOR EACH ROW 
+ BEGIN
+  SET NEW.created_at = NOW();
+  SET NEW.updated_at = NOW();
+END $$
+DELIMITER ;
+
+
+-- trigger untuk mengatur tanggal saat insert data pada tabel materials
+DELIMITER $$
+CREATE TRIGGER `tanggal-materials` BEFORE INSERT ON `materials`
  FOR EACH ROW 
  BEGIN
   SET NEW.created_at = NOW();
