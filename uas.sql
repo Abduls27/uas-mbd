447f1d43a1a748f25473191bc53d0d30e015f2b1
Abduls27 <fahmiabdullah628@gmail.com>
2024-06-26 21:38:55 +0700

    modified:   uas.sql

--- uas.sql
+++ uas.sql
@@ -28,3 +28,29 @@ CREATE TRIGGER `tanggal-materials` BEFORE INSERT ON `materials`
   SET NEW.updated_at = NOW();
 END $$
 DELIMITER ;
+
+-- stored procedure untuk mencari judul dan kategori modul
+DELIMITER $$
+CREATE PROCEDURE selectmodule()
+BEGIN
+    SELECT title, category FROM modules;
+END$$
+DELIMITER ;
+
+
+-- stored procedure untuk mencari judul dan id submodul material
+DELIMITER $$ 
+CREATE PROCEDURE selectmaterial()
+BEGIN
+    SELECT title, id_submodule FROM materials;
+END$$
+DELIMITER ;
+
+
+-- stored procedure untuk mencari nama email dan jenis autentifikasi user
+DELIMITER $$
+CREATE PROCEDURE selectuser()
+BEGIN
+    SELECT name, email, auth_type FROM users;
+END$$
+DELIMITER ;
